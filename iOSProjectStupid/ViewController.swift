eefa0ba288f62b5965deb32c25e97d9bc7e6ceb6
Rinzler838 <dvil4688@740s-pl-105.local>
2016-01-27 09:32:09 -0700

    Misc

--- iOSProjectStupid/ViewController.swift
+++ iOSProjectStupid/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeColor(sender: AnyObject)
+    {
+        view.backgroundColor = UIColor.blackColor()
+    }
+        
+    func randomColorChange()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
